f78a009ac6a6a5b9d1a7072c21add25306018a6f
twkim5235 <sssunggure@naver.com>
2021-11-11 00:27:13 +0900

    [FIX] 1:1 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -21,6 +21,10 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
